7085c92e34deaa033f0dd3dcda9fffa0e2a478fa
Vishal-786 <142048039+Vishal-786@users.noreply.github.com>
2025-05-04 22:44:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ nltk_data_path = os.path.join(os.path.dirname(__file__), 'nltk_data')
 nltk.data.path.append(nltk_data_path)
 
 # Manually download required data
-nltk.download('punkt', download_dir=nltk_data_path)
+nltk.download('punkt_tab', download_dir=nltk_data_path)
 nltk.download('stopwords', download_dir=nltk_data_path)
 nltk.download('wordnet', download_dir=nltk_data_path)
 
