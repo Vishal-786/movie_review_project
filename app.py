22c2bcbc7af6b37dd8f466bc970e066acdcab984
Vishal Kanyal <vishalkanyal786@gmail.com>
2025-05-13 16:22:35 +0530

    update

--- app.py
+++ app.py
@@ -12,13 +12,16 @@ from gensim.models import Word2Vec
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import numpy as np 
 
+import nltk
+import streamlit as st
+
 # Function to download necessary NLTK resources
 def download_nltk_resources():
     try:
         nltk.data.find('tokenizers/punkt')
     except LookupError:
         try:
-            nltk.download('punkt')
+            nltk.download('punkt', download_dir='/tmp/nltk_data')
         except Exception as e:
             st.error(f"Failed to download NLTK punkt: {e}")
 
@@ -26,7 +29,7 @@ def download_nltk_resources():
         nltk.data.find('corpora/stopwords')
     except LookupError:
         try:
-            nltk.download('stopwords')
+            nltk.download('stopwords', download_dir='/tmp/nltk_data')
         except Exception as e:
             st.error(f"Failed to download NLTK stopwords: {e}")
 
@@ -34,7 +37,7 @@ def download_nltk_resources():
         nltk.data.find('corpora/wordnet')
     except LookupError:
         try:
-            nltk.download('wordnet')
+            nltk.download('wordnet', download_dir='/tmp/nltk_data')
         except Exception as e:
             st.error(f"Failed to download NLTK wordnet: {e}")
 
